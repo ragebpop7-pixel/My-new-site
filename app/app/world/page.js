91acc72bbfd1e33a725dab1b3c8de4cb2b008f6c
ragebpop7-pixel <ragebpop7@gmail.com>
2026-05-03 18:01:42 +0300

    Update page.js

--- app/app/world/page.js
+++ app/app/world/page.js
@@ -1,2 +1,8 @@
-top-headlines
-40a916fb34df43ea9776e1e50715663b
+export default function Sudan() {
+  return (
+    <div>
+      <h1>أخبار السودان</h1>
+      <p>هنا هيظهر الأخبار لاحقًا</p>
+    </div>
+  );
+}
