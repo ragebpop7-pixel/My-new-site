9bcf165a80eca7e2948877ed10dd2e93cf7fb2b2
ragebpop7-pixel <ragebpop7@gmail.com>
2026-05-03 18:14:12 +0300

    Update page.js

--- app/app/egypt/page.js
+++ app/app/egypt/page.js
@@ -1,3 +1,34 @@
-export default function Egypt() {
-  return <h1>🇪🇬 أخبار مصر</h1>;
+async function getNews() {
+  const res = await fetch(
+    "https://newsapi.org/v2/everything?q=egypt&apiKey=40a916fb34df43ea9776e1e50715663b",
+    { cache: "no-store" }
+  );
+
+  const data = await res.json();
+  return data.articles;
+}
+
+export default async function Egypt() {
+  const news = await getNews();
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>🇪🇬 أخبار مصر</h1>
+
+      {news.map((item, i) => (
+        <div key={i} style={{ marginBottom: 20 }}>
+          <h3>{item.title}</h3>
+          <p>{item.description}</p>
+
+          {item.urlToImage && (
+            <img src={item.urlToImage} width="100%" />
+          )}
+
+          <a href={item.url} target="_blank">
+            اقرأ الخبر
+          </a>
+        </div>
+      ))}
+    </div>
+  );
 }
